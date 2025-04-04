7e4a1685bb958a03dee82987534c9560949d6a4d
Sandesh <sandeshac2@gmail.com>
2025-04-04 16:16:14 +0530

    Initial Commit

---
+++ Vespel/VespelApp.swift
@@ -0,0 +1,17 @@
+//
+//  VespelApp.swift
+//  Vespel
+//
+//  Created by Sandesh Raj on 04/04/25.
+//
+
+import SwiftUI
+
+@main
+struct VespelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VespelTests/VespelTests.swift
@@ -0,0 +1,17 @@
+//
+//  VespelTests.swift
+//  VespelTests
+//
+//  Created by Sandesh Raj on 04/04/25.
+//
+
+import Testing
+@testable import Vespel
+
+struct VespelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
